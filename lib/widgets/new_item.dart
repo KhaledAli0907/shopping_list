1ce353d12c178e0f0feb8273437a524a8e04fbf9
khaledali0907 <khaledali0907@gmail.com>
2026-04-17 14:55:54 +0200

    first commit

---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+
+class NewItemScreen extends StatefulWidget {
+  const NewItemScreen({super.key});
+
+  @override
+  State<NewItemScreen> createState() => _NewItemScreenState();
+}
+
+class _NewItemScreenState extends State<NewItemScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String? _enteredName = '';
+  int? _enteredQuantity = 1;
+  Category? _selectedCategory = categories[Categories.other]!;
+
+  String? _validateInput(String? value, {required bool isQuantity}) {
+    if (value == null || value.isEmpty) {
+      return isQuantity
+          ? 'Quantity must be a positive number'
+          : 'Name must be between 2 and 50 characters long';
+    }
+
+    if (isQuantity) {
+      final parsedValue = int.tryParse(value);
+      if (parsedValue == null || parsedValue <= 0) {
+        return 'Quantity must be a positive number';
+      }
+      return null;
+    }
+
+    final trimmedValue = value.trim();
+    if (trimmedValue.length <= 1 || trimmedValue.length > 50) {
+      return 'Name must be between 2 and 50 characters long';
+    }
+    return null;
+  }
+
+  void _saveItem() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: DateTime.now().toIso8601String(),
+          name: _enteredName!,
+          quantity: _enteredQuantity!,
+          category: _selectedCategory!,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Add a new item')),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(labelText: 'Name'),
+                validator: (value) => _validateInput(value, isQuantity: false),
+                onSaved: (value) {
+                  _enteredName = value!;
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Quantity'),
+                      initialValue: '1',
+                      keyboardType: TextInputType.number,
+                      validator: (value) =>
+                          _validateInput(value, isQuantity: true),
+                      onSaved: (value) {
+                        _enteredQuantity = int.parse(value!);
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      initialValue: _selectedCategory,
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(width: 6),
+                                Text(category.value.name),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedCategory = value;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
+                    child: const Text('Reset'),
+                  ),
+                  TextButton(
+                    onPressed: _saveItem,
+                    child: const Text('Add Item'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
